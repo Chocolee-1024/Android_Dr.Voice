199a0f3d38425f110096abd6bf3e179d1a5d5f86
isa <willin14687@gmail.com>
2016-08-24 16:12:35 +0800

    add Framework

---
+++ app/src/main/java/com/imac/voice_app/util/MainActivity.java
@@ -0,0 +1,15 @@
+package com.imac.voice_app.util;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.imac.voice_app.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
