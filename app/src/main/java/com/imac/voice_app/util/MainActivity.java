e62ea0cf50c4be1905c95ef6f70978e2cd3ee503
isa <willin14687@gmail.com>
2016-08-25 10:52:40 +0800

    add butterKnife autofittextview

--- app/src/main/java/com/imac/voice_app/util/MainActivity.java
+++ app/src/main/java/com/imac/voice_app/util/MainActivity.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 
 import com.imac.voice_app.R;
 
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
