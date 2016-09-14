0116502502e04594df79749cd8f52e7b5915fda2
黃煒哲 <1410232038@email.com>
2016-09-14 17:56:27 +0800

    完成BugFix 加上Toolbar

---
+++ app/src/main/java/com/imac/voice_app/component/ToolbarView.java
@@ -0,0 +1,88 @@
+package com.imac.voice_app.component;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.imac.voice_app.R;
+import com.imac.voice_app.core.ActivityLauncher;
+import com.imac.voice_app.util.mainmenu.MainActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * Toolbar view，custom attr in xml for：
+ * backButtonVisibility format="boolean"
+ * titleText format="string"
+ * Created by flowmaHuang on 2016/9/13.
+ */
+public class ToolbarView extends RelativeLayout {
+    @BindView(R.id.iv_back_left_arrow)
+    ImageView backButton;
+    @BindView(R.id.iv_menu)
+    ImageView menuButton;
+    @BindView(R.id.tv_title)
+    TextView titleTextView;
+
+    public interface toolbarCallBack {
+        String titleText();
+
+        void backButtonListener();
+    }
+
+    private toolbarCallBack listener = null;
+    private Context mContext;
+    private View view;
+
+    public ToolbarView(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+        mContext = context;
+
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        view = inflater.inflate(R.layout.layout_toolbar, null, false);
+        ButterKnife.bind(this,view);
+        addView(view);
+
+        loadStateFromAttrs(attributeSet);
+
+    }
+
+    private void loadStateFromAttrs(AttributeSet attributeSet) {
+        if (attributeSet == null) {
+            return; // quick exit
+        }
+
+        TypedArray a = null;
+        try {
+            a = getContext().obtainStyledAttributes(attributeSet, R.styleable.ToolbarView);
+            if (!a.getBoolean(R.styleable.ToolbarView_backButtonVisibility, false)) {
+                backButton.setVisibility(INVISIBLE);
+            }
+            titleTextView.setText(a.getText(R.styleable.ToolbarView_titleText));
+        } finally {
+            if (a != null) {
+                a.recycle(); // ensure this is always called
+            }
+        }
+    }
+
+    @OnClick({R.id.iv_back_left_arrow, R.id.iv_menu})
+    public void onClick(ImageView iv) {
+        switch (iv.getId()) {
+            case R.id.iv_back_left_arrow:
+//                listener.backButtonListener();
+                titleTextView.setText("99999");
+                break;
+            case R.id.iv_menu:
+                ActivityLauncher.go(mContext, MainActivity.class, null);
+                break;
+        }
+    }
+}
